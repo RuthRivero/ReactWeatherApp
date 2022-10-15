541380f8a90abcce2e029fc9695abdd1e0a1ca1c
James Robertson <110299906+RuthRivero@users.noreply.github.com>
2022-10-15 09:10:57 -0400

    add body root

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,16 +1,29 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Forecast() {
   return (
-    <div>
-      <div class="col-sm-2">
-        <div class="day">Sun</div>
-        <div class="temp">12</div>
-      </div>
-
-      <div class="col-sm-2">
-        <div class="day">Mon</div>
-        <div class="temp">12</div>
+    <div className="forecast">
+      <div class="row">
+        <div class="col-sm-2">
+          <div class="day">Sun</div>
+          <span class="emoji">
+            <img src="☀️" alt="arial-label" />
+          </span>
+          <div class="temp">12</div>
+        </div>
+        <div class="col-sm-2">
+          <div class="day">Mon</div>
+          <div class="temp">12</div>
+        </div>
+        <div class="col-sm-2">
+          <div class="day">Tue</div>
+          <div class="temp">12</div>
+        </div>
+        <div class="col-sm-2">
+          <div class="day">Wed</div>
+          <div class="temp">12</div>
+        </div>
       </div>
     </div>
   );
